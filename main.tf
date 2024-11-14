ad96be602991819d79e16c3e268ae9edf3121e99
dhia-zeddini <dhia.zeddini.14@gmail.com>
2024-11-14 01:34:36 +0100

    update prometheus config v4.1

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "docker_container" "prometheus" {
   }
 
   mounts {
-    source = "${path.module}/prometheus.yml"
+    source = "${abspath(path.module)}/prometheus.yml"
     target = "/etc/prometheus/prometheus.yml"
     type   = "bind"
   }
